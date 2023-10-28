7abeb9004d113d386feaeb94794febca1e0a2dc0
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 17:50:27 -0400

    added components and imports navLink including props

---
+++ MyPortfolio/src/Components/NavbarLink.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export const NavbarLink = ({ closeNavbar, path, children }) => {
+  return (
+    <Nav.Link
+      className="mx-auto text-light"
+      as={Link}
+      to={path}
+      onClick={closeNavbar}
+    >
+      {children}
+    </Nav.Link>
+  );
+};
