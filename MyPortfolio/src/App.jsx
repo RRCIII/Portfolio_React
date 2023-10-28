47f6849e2f5ace6d706db07de9316aaef479651e
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 17:26:16 -0400

    updated spacing

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-no-target-blank */
 import { useEffect } from 'react'
 const App = () => {
-  //hook applied for cookie to be rendered once when browser is refreshed with cross-origin applied
+  //side-effect/hook applied for cookie to be rendered once when browser is refreshed with cross-origin applied
   useEffect(() =>{
     document.cookie = "myCookie=myValue; SameSite=None; Secure";
   }, [])
+  
   return ( 
     <div className="main-container"></div>
 
