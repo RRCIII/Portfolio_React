2f9c29e2ef69e09807187a0a3510b134e344a4c7
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 17:21:10 -0400

    updated app.jsx comment

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-target-blank */
 import { useEffect } from 'react'
 const App = () => {
-  //hook applied for cookie to be rendered once when browser is refreshed
+  //hook applied for cookie to be rendered once when browser is refreshed with cross-origin applied
   useEffect(() =>{
     document.cookie = "myCookie=myValue; SameSite=None; Secure";
   }, [])
