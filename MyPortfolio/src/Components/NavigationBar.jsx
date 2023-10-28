7350e0770024b8baf3b2e173c264bd2ece77bdc5
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 17:47:18 -0400

    added components with props for navbar fn

--- MyPortfolio/src/Components/NavigationBar.jsx
+++ MyPortfolio/src/Components/NavigationBar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import { useState } from 'react';
 import { NavbarLink } from "./Navbar/NavbarLink";
 import { Container, Nav, Navbar } from "react-bootstrap";
@@ -15,7 +17,44 @@ const NavigationBar = () => {
     return ( 
         <>
         <Navbar
-     );
-}
+          expand="md"
+          className="bg-primary"
+          fixed="top"
+          expanded={expanded}
+        >
+          <Container fluid className="px-5">
+            <Navbar.Brand
+              className="fs-1 text-light"
+              as={Link}
+              to="/"
+              onClick={() => setExpanded(false)}
+            >
+              <FontAwesomeIcon icon={faCameraRetro} />
+            </Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              onClick={() => setExpanded(expanded ? false : "expanded")}
+            />
+            <Navbar.Collapse id="basic-navbar-nav text-light">
+              <Nav className="ms-auto fs-6 nav-fg">
+                <NavbarLink closeNavbar={closeNavbar} path="/about">
+                  About Me
+                </NavbarLink>
+                <NavbarLink closeNavbar={closeNavbar} path="/portfolio">
+                  Portfolio
+                </NavbarLink>
+                <NavbarLink closeNavbar={closeNavbar} path="/contact">
+                  Contact Me
+                </NavbarLink>
+                <NavbarLink closeNavbar={closeNavbar} path="/resume">
+                  Resume
+                </NavbarLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
+    );
+};
  
 export default NavigationBar;
