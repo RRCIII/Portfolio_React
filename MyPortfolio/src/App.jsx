de15410347995b62a6e0dc9f5aab61e13325defa
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 18:43:53 -0400

    updated imports for usage

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/jsx-no-target-blank */
 import { useEffect } from 'react'
+import './sass/App.scss'
+import NavigationBar from './Components/NavigationBar'
+import { Outlet } from 'react-router-dom'
+
 const App = () => {
   //side-effect/hook applied for cookie to be rendered once when browser is refreshed with cross-origin applied
   useEffect(() =>{
