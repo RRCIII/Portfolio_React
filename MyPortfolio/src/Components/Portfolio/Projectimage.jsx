67a4bc641308410d254f3989be77b2408dc678a3
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 18:30:47 -0400

    created project image  file prop fn

---
+++ MyPortfolio/src/Components/Portfolio/Projectimage.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Col } from "react-bootstrap";
+
+const ProjectImage = ({
+  handleMouseEnter,
+  handleMouseLeave,
+  title,
+  children,
+}) => {
+  return (
+    <Col
+      md={4}
+      className="d-flex justify-content-center"
+      style={{ margin: "1.1rem", width: "360px" }}
+    >
+      <div
+        className="img-container"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="img-contain position-relative">{children}</div>
+
+        <h5 className="mt-2 text-secondary-emphasis">{title}</h5>
+      </div>
+    </Col>
+  );
+};
+
+export default ProjectImage;
