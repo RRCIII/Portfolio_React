bdb8f610e224cad56304fc8c278de25ce60e3e58
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 17:18:56 -0400

    updated app.jsx with hook for page rendering

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/jsx-no-target-blank */
+import { useEffect } from 'react'
+const App = () => {
+  //hook applied for cookie to be rendered once when browser is refreshed
+  useEffect(() =>{
+    document.cookie = "myCookie=myValue; SameSite=None; Secure";
+  }, [])
+  return ( 
+    <div className="main-container"></div>
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
-
+ 
 export default App
