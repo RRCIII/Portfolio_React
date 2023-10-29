5dc3833f5c10340d80827c0f2fd646faedf85bd4
Ricky Carter <randolphcarter5@gmail.com>
2023-10-28 23:36:41 -0400

    updated navigationBar.jsx

--- MyPortfolio/src/Components/NavigationBar.jsx
+++ MyPortfolio/src/Components/NavigationBar.jsx
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 
 const NavigationBar = () => {
-    const [ expand, setExpanded] = useState(false);
+    const [ expanded, setExpanded] = useState(false);
 
     const closeNavbar = () => {
         setExpanded(false);
